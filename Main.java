eebd16026263b456e522c7bfdf915341741829ab
Kuzya <kuzyaki48@gmail.com>
2026-03-27 14:27:11 +0500

    Добавил Nginx в качестве обратного прокси

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, DevOps World! Моя Java-Программа Работает!");
-	}
+    public static void main(String[] args) throws IOException {
+        // Создаем сервер, который будет слушать порт 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        
+        // Говорим, что отвечать, когда кто-то заходит на главную страницу ("/")
+        server.createContext("/", exchange -> {
+            String response = "Privet! Eto moy perviy Java web-server v Docker!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        
+        // Запускаем сервер
+        server.start();
+        System.out.println("Сервер запущен и работает на порту 8080...");
+    }
 }
